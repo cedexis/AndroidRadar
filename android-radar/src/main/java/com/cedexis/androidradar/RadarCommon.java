7abfd581f83728add74a3a8b2187951463da7a4f
jmsalcido <eljose152@gmail.com>
2017-02-23 12:30:49 -0700

    send network headers via http params

--- android-radar/src/main/java/com/cedexis/androidradar/RadarCommon.java
+++ android-radar/src/main/java/com/cedexis/androidradar/RadarCommon.java
@@ -13,16 +13,10 @@ public class RadarCommon {
     private final static String WTYPE = "WTYPE";
     public static final String WWAN_TYPE = "WWAN_TYPE";
 
-    /**
-     * To get device consuming netowkr type is 2g,3g,4g
-     *
-     * @param context
-     * @return "2g","3g","4g" as a String based on the network type
-     */
     public String getNetworkType(Context context) {
-        TelephonyManager mTelephonyManager = (TelephonyManager)
+        TelephonyManager telephonyManager = (TelephonyManager)
                 context.getSystemService(Context.TELEPHONY_SERVICE);
-        int networkType = mTelephonyManager.getNetworkType();
+        int networkType = telephonyManager.getNetworkType();
         switch (networkType) {
             case TelephonyManager.NETWORK_TYPE_GPRS:
             case TelephonyManager.NETWORK_TYPE_EDGE:
@@ -33,30 +27,14 @@ public class RadarCommon {
             case TelephonyManager.NETWORK_TYPE_UMTS:
             case TelephonyManager.NETWORK_TYPE_EVDO_0:
             case TelephonyManager.NETWORK_TYPE_EVDO_A:
-                /**
-                 From this link https://goo.gl/R2HOjR ..NETWORK_TYPE_EVDO_0 & NETWORK_TYPE_EVDO_A
-                 EV-DO is an evolution of the CDMA2000 (IS-2000) standard that supports high data rates.
-
-                 Where CDMA2000 https://goo.gl/1y10WI .CDMA2000 is a family of 3G[1] mobile technology standards for sending voice,
-                 data, and signaling data between mobile phones and cell sites.
-                 */
             case TelephonyManager.NETWORK_TYPE_HSDPA:
             case TelephonyManager.NETWORK_TYPE_HSUPA:
             case TelephonyManager.NETWORK_TYPE_HSPA:
             case TelephonyManager.NETWORK_TYPE_EVDO_B:
             case TelephonyManager.NETWORK_TYPE_EHRPD:
             case TelephonyManager.NETWORK_TYPE_HSPAP:
-                //Log.d("Type", "3g");
-                //For 3g HSDPA , HSPAP(HSPA+) are main  networktype which are under 3g Network
-                //But from other constants also it will 3g like HSPA,HSDPA etc which are in 3g case.
-                //Some cases are added after  testing(real) in device with 3g enable data
-                //and speed also matters to decide 3g network type
-                //http://goo.gl/bhtVT
                 return "3G";
             case TelephonyManager.NETWORK_TYPE_LTE:
-                //No specification for the 4g but from wiki
-                //I found(LTE (Long-Term Evolution, commonly marketed as 4G LTE))
-                //https://goo.gl/9t7yrR
                 return "4G";
             default:
                 return "Notfound";
